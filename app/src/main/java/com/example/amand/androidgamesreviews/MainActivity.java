a04595d446427fefdeac5b627d4d8c78fc2e4ab2
André Amândio <amandio93@hotmail.com>
2016-01-24 17:11:24 +0000

    v2 - Primeiro Layout, Tema do projeto

--- app/src/main/java/com/example/amand/androidgamesreviews/MainActivity.java
+++ app/src/main/java/com/example/amand/androidgamesreviews/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.amand.androidgamesreviews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnGameList_OnClick(View view){
+        Intent fragmentLocal = new Intent(this, FragmentContainer.class);
+        fragmentLocal.putExtra("btnName", "gameList");
+        startActivity(fragmentLocal);
+    }
+
+    public void btnCategory_OnClick(View view){
+        Intent fragmentLocal = new Intent(this, FragmentContainer.class);
+        fragmentLocal.putExtra("btnName", "categoria");
+        startActivity(fragmentLocal);
+    }
+
+    public void btnProdution_OnClick(View view){
+        Intent fragmentLocal = new Intent(this, FragmentContainer.class);
+        fragmentLocal.putExtra("btnName", "produtora");
+        startActivity(fragmentLocal);
     }
 }
